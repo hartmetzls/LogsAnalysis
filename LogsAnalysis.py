d40b14f7916e2198e85a7eee8f6e42ddb592c395
hartmetzls <hartmetzls@gmail.com>
2017-08-06 03:13:22 -0400

    message still occurs every time I have multiple select queries: <built-in method fetchall of psycopg2.extensions.cursor object at 0xb69071ac>

--- LogsAnalysis.py
+++ LogsAnalysis.py
@@ -4,7 +4,17 @@ import psycopg2
 
 DBNAME = "news"
 
-def create_top_paths():
+# It was recommended to me by a Udacity mentor in the live chat to use something like this to execute all of my queries,
+#   but I haven't figured out how to utilize it:
+# def execute_query(query):
+#     db = psycopg2.connect(database=DBNAME)
+#     c = db.cursor()
+#     c.execute(query)
+#     results = c.fetchall
+#     db.close()
+#     return results
+
+def top_articles():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
     # copy pastable format for debugging with psql console
@@ -21,30 +31,16 @@ def create_top_paths():
               "FROM articles JOIN top_paths "
               "ON top_paths.path = '/article/' || articles.slug;")
     articles = c.fetchall()
-    # title1 = articles[0][0] #[[title, num]...] #print title1 <--first steps for formatting to plain text
+    print articles
+    # c.execute("CREATE VIEW popular_authors AS "
+    #           "SELECT author, count (log.path) AS views "
+    #           "FROM articles JOIN log "
+    #           "ON log.path = '/article/' || articles.slug "
+    #           "GROUP BY author "
+    #           "ORDER BY views desc;")
     db.close()  # when you close, it gets rid of your views
-    return articles
-
-def popular_authors():
-    print "cheese"
-    db = psycopg2.connect(database=DBNAME)
-    c = db.cursor()
-
-    # copy pastable format for debugging with psql console
-    c.execute("CREATE VIEW popular_authors AS "
-              "SELECT author, count (log.path) AS views "
-              "FROM articles JOIN log "
-              "ON log.path = '/article/' || articles.slug "
-              "GROUP BY author "
-              "ORDER BY views desc;")
-    c.execute("SELECT * FROM popular_authors;")
-    pop_auths = c.fetchall
-    db.close()
-    return pop_auths
-
 
 if __name__ == '__main__':
-    print create_top_paths()
-    print popular_authors()
+    top_articles()
 
-
+# title1 = articles[0][0] #[[title, num]...] #print title1 <--first steps for formatting to plain text
