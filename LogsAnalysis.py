54129863d34ef06a46fe1d42281c95469db16fdd
hartmetzls <hartmetzls@gmail.com>
2017-08-07 23:41:34 -0400

    Finally successfully doing multiple queries in one function!

--- LogsAnalysis.py
+++ LogsAnalysis.py
@@ -17,9 +17,6 @@ DBNAME = "news"
 def top_articles_and_authors():
     db = psycopg2.connect(database=DBNAME)
     c = db.cursor()
-    # copy pastable format for debugging with psql console
-    # CREATE VIEW top_paths AS SELECT path, count(log.path) as num FROM log WHERE path LIKE '%article%' GROUP BY path
-    # ORDER BY num DESC LIMIT 3;
     c.execute("CREATE VIEW top_paths AS "
               "SELECT path, count(log.path) as num "
               "FROM log "
@@ -37,11 +34,30 @@ def top_articles_and_authors():
               "FROM articles JOIN log "
               "ON log.path = '/article/' || articles.slug "
               "GROUP BY author "
-              "ORDER BY views desc;")
+              "ORDER BY views DESC;")
     c.execute("SELECT * FROM popular_authors;")
     authors = c.fetchall()
     print authors
-    db.close()  # when you close, it gets rid of your views
+    # create a view that has dates and each date's statuses count
+    # create view with dates and each date's 404 count
+    # if count 404/count total status > .01: list day
+    c.execute("CREATE VIEW dates_and_statuses AS "
+              "SELECT time::timestamp::date AS date, count (log.status) AS stat_count "
+              "FROM log "
+              "GROUP BY date;")
+    c.execute("CREATE VIEW dates_and_404s AS "
+              "SELECT time::timestamp::date AS date, count (log.status) AS errors "
+              "FROM log "
+              "WHERE status LIKE '%404%' "
+              "GROUP BY date;")
+    c.execute("CREATE VIEW dates_and_percentages AS "
+              "SELECT dates_and_statuses.date, dates_and_404s.errors / dates_and_statuses.stat_count ::float AS percentage "
+              "FROM dates_and_404s JOIN dates_and_statuses "
+              "ON dates_and_404s.date = dates_and_statuses.date;")
+    c.execute("SELECT * from dates_and_percentages;")
+    d_and_s = c.fetchall()
+    print d_and_s
+    db.close()
 
 if __name__ == '__main__':
     top_articles_and_authors()
