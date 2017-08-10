58c8ff0d470954e871d636008b94339e2addbd31
hartmetzls <hartmetzls@gmail.com>
2017-08-10 16:54:46 -0400

    Version for 2nd project submission

--- LogsAnalysis.py
+++ LogsAnalysis.py
@@ -22,8 +22,10 @@ def popular_articles():
               "ON top_paths.path = '/article/' || articles.slug "
               "ORDER BY num DESC;")
     articles = c.fetchall()
+    print ""
+    print "What are the three most popular articles?\n"
     for a in articles:
-        print a[0] + " - " + str(a[1])[:-1] + " views"
+        print "\t" + a[0] + " - " + str(a[1]) + " views"
     db.close()
 
 
@@ -42,8 +44,10 @@ def popular_authors():
               "ON authors.id = popular_authors.author "
               "ORDER BY popular_authors.views DESC;")
     authors = c.fetchall()
+    print ""
+    print "Who are the most popular authors?\n"
     for a in authors:
-        print a[0] + " - " + str(a[1])[:-1] + " views"
+        print "\t" + a[0] + " - " + str(a[1]) + " views"
     db.close()
 
 
@@ -52,25 +56,32 @@ def days_with_high_error_percentage():
     c = db.cursor()
     c.execute("DROP VIEW IF EXISTS dates_and_statuses;")
     c.execute("CREATE VIEW dates_and_statuses AS "
-              "SELECT time::timestamp::date AS date, count (log.status) AS stat_count "
+              "SELECT time::date AS date, "
+              "count (log.status) AS stat_count "
               "FROM log "
               "GROUP BY date;")
     c.execute("DROP VIEW IF EXISTS dates_and_404s;")
     c.execute("CREATE VIEW dates_and_404s AS "
-              "SELECT time::timestamp::date AS date, count (log.status) AS errors "
+              "SELECT time::date AS date, "
+              "count (log.status) AS errors "
               "FROM log "
               "WHERE status LIKE '%404%' "
               "GROUP BY date;")
     c.execute("DROP VIEW IF EXISTS dates_and_percentages;")
     c.execute("CREATE VIEW dates_and_percentages AS "
               "SELECT dates_and_statuses.date, "
-              "dates_and_404s.errors / dates_and_statuses.stat_count ::float AS percentage "
+              "dates_and_404s.errors / dates_and_statuses.stat_count::float "
+              "AS percentage "
               "FROM dates_and_404s JOIN dates_and_statuses "
               "ON dates_and_404s.date = dates_and_statuses.date;")
-    c.execute("SELECT to_char(date, 'Month DD YYYY'), percentage from dates_and_percentages WHERE percentage > .01;")
+    c.execute("SELECT to_char(date, 'FMMonth FMDD YYYY'), "
+              "percentage from dates_and_percentages WHERE percentage > .01;")
     dates = c.fetchall()
+    print ""
+    print "On what days did the error percentage rate exceed 1%?\n"
     for a in dates:
-        print a[0] + " - " + "{:.1%}".format(a[1])
+        print "\t" + a[0] + " - " + "{:.1%}".format(a[1])
+    print ""
     db.close()
 
 if __name__ == '__main__':
